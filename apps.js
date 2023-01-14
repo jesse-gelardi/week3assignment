6f4469958a2d1a2ef56db51ffc30f8e273643f77
Jesse Gelardi <jesse.gelardi97@gmail.com>
2023-01-14 13:50:57 -0500

    third commit

--- apps.js
+++ apps.js
@@ -1,14 +1,26 @@
 
 console.log('Hello!');
 
-        
-  //All questions must be printed to your Browser’s console:
-    //1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
-    //let agesArray = agesArray.sort([3, 9 , 23, 64, 2, 8, 28, 93]);
+//1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+const ages = [
+    '3',
+    '9',
+    '23',
+    '64',
+    '2',
+    '8',
+    '28',
+    '93',
+  ]; 
+  //let agesArray = agesArray.sort([3, 9 , 23, 64, 2, 8, 28, 93]);
+  let agesArray = agesArray.sort([3, 9 , 23, 64, 2, 8, 28, 93]);
+
         //1a. Programmatically subtract the value of the first element in the array from the value in the last element of the array 
             //▪ Do not use numbers to reference the last element, find it programmatically, 
           // ▪ ages[7] – ages[0] is not allowed!
           //console.log(agesArray);
+
+          
 
         //1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
         //1c. Use a loop to iterate through the array and calculate the average age. 
